4f144b6b7b69aec265096ed5038bb04747fb3a88
FrancoisLaumond <laumondfrancois@gmail.com>
2025-01-29 12:27:24 +0100

    Update admin.php title to 'Administrator Page'

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tableau avec Emoticônes</title>
+    <title>Administrator Page</title>
     <script src="back-script.js"></script>
     <link rel="stylesheet" href="back-style.css">
 </head>
