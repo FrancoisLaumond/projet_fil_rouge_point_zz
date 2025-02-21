968a71a6fa3cae77facc730d040bf6880b671cf0
marsooooo <marso.tison@gmail.com>
2025-02-21 16:03:36 +0100

    update footer style

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <p>Merci de soutenir l'artisanat français</p>
     <div class="footer">
         <div><a href="#"><img src="content/index1.png" alt="value1"></a></div>
-        <a href="#"><h4>Categories</h4></a><a href="#">Sac</a><a href="#">Pochette</a><a href="#">Objet du quotidien</a><a href="#">Bébé</a><a href="#">Vêtements</a>
+        <h4><a href="#">Categories</h4></a><h5><a href="#">Sac</a></h5><h5><a href="#">Pochette</a></h5><h5><a href="#">Objet du quotidien</a></h5><h5><a href="#">Bébé</a></h5><h5><a href="#">Vêtements</a></h5>
         <div><a href="#"><h4>Conception</h4></a><a href="#"><h4>Contact</h4></a><a href="#"><h4>Mentions légales</h4></a></div>
     </div>
 </footer>
