c13b1ec329c7ee5562eabd400541c4530e45cdbc
marsooooo <marso.tison@gmail.com>
2025-03-03 19:17:24 +0100

    nav tab hover extension

--- nav.php
+++ nav.php
@@ -1,7 +1,27 @@
 <nav>
     <a href="index.php" class="navLogo"><img src="logo.png" alt="Logo"></a>
     <ul>
-        <li><a href="article.php">Mes créations</a></li>
+        <li class="dropdown">
+            <a href="article.php">Mes créations</a>
+            <div class="submenu">
+                <img src="index1.png" alt="dropdown image">
+                <div class="dropdown-list">
+                    <h2>Mes Créations</h2>
+                    <div>
+                        <div class="dropdown-sublist">
+                            <h5><a href="">Tout voir</a></h5>
+                            <h5><a href="">Gamme bébé</a></h5>
+                            <h5><a href="">Gamme femme</a></h5>
+                        </div>
+                        <div class="dropdown-sublist">
+                            <h5><a href="">Gamme zéro déchet</a></h5>
+                            <h5><a href="">Sacs à dos</a></h5>
+                            <h5><a href="">Sac banane</a></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
         <li><a href="about.php">Conception</a></li>
         <li><a href="contact.php" class="contactButton">Contact</a></li>
     </ul>
